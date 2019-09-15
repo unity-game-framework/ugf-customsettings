f8c8e7bc88a8e95eb34e4adb91e838fcc59c445e
Alexander <vorobyovvorobyov@gmail.com>
2019-09-15 21:08:11 +0300

    Update CustomSettingsGUIScope.cs

--- Packages/UGF.CustomSettings/Editor/CustomSettingsGUIScope.cs
+++ Packages/UGF.CustomSettings/Editor/CustomSettingsGUIScope.cs
@@ -3,11 +3,21 @@ using UnityEditor;
 
 namespace UGF.CustomSettings.Editor
 {
+    /// <summary>
+    /// Represents the GUI scope used to draw editor in project settings.
+    /// </summary>
+    /// <remarks>
+    /// The main purposes to setup settings to be the same as during drawing editor in 'Inspector' window.
+    /// </remarks>
     public class CustomSettingsGUIScope : IDisposable
     {
         private readonly bool m_hierarchyMode;
         private readonly bool m_wideMode;
 
+        /// <summary>
+        /// Creates scope with the specified enabled argument.
+        /// </summary>
+        /// <param name="enable"></param>
         public CustomSettingsGUIScope(bool enable = true)
         {
             m_hierarchyMode = EditorGUIUtility.hierarchyMode;
