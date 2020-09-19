22014aa3d5ea6a5b52e31f73e4642060cc15d341
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-09-20 02:29:01 +0300

    Add TestSettingsPackageWindow

---
+++ Assets/UGF.CustomSettings.Editor.Tests/TestSettingsPackageWindow.cs
@@ -0,0 +1,20 @@
+﻿using UGF.CustomSettings.Runtime;
+using UGF.CustomSettings.Runtime.Tests;
+using UnityEditor;
+
+namespace UGF.CustomSettings.Editor.Tests
+{
+    public class TestSettingsPackageWindow : CustomSettingsWindow<TestSettingsData>
+    {
+        [MenuItem("Tests/TestSettingsPackageWindow")]
+        private static void Menu()
+        {
+            GetWindow<TestSettingsPackageWindow>(false, "Settings");
+        }
+
+        protected override CustomSettings<TestSettingsData> OnGetSettings()
+        {
+            return TestSettingsPackage.Settings;
+        }
+    }
+}
