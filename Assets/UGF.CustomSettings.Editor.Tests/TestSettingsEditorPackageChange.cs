672badf86df5d1d65611182d67d441937b731f9d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-10 21:45:57 +0300

    Update tests

---
+++ Assets/UGF.CustomSettings.Editor.Tests/TestSettingsEditorPackageChange.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using UGF.EditorTools.Editor.Yaml;
+using UnityEditor;
+
+namespace UGF.CustomSettings.Editor.Tests
+{
+    public class TestSettingsEditorPackageChange
+    {
+        private string m_previousName;
+        private const string PATH = "Assets/Settings/Editor/UGF.Test.Editor.Package/TestEditorPackageSettings.asset";
+
+        [OneTimeSetUp]
+        public void SetupAll()
+        {
+            var data = EditorYamlUtility.FromYamlAtPath<TestSettingsEditorData>(PATH);
+
+            m_previousName = data.Name;
+        }
+
+        [OneTimeTearDown]
+        public void TeardownAll()
+        {
+            var data = EditorYamlUtility.FromYamlAtPath<TestSettingsEditorData>(PATH);
+
+            data.Name = m_previousName;
+
+            EditorYamlUtility.ToYamlAtPath(data, PATH);
+            AssetDatabase.ImportAsset(PATH);
+            AssetDatabase.SaveAssets();
+        }
+
+        [Test]
+        public void Change()
+        {
+            string name = "Test Name Change";
+            TestSettingsEditorPackage.Settings.Data.Name = name;
+            TestSettingsEditorPackage.Settings.SaveSettings();
+
+            var data = EditorYamlUtility.FromYamlAtPath<TestSettingsEditorData>(PATH);
+
+            Assert.AreEqual(name, data.Name);
+        }
+    }
+}
