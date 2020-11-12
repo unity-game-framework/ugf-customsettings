74943385ea0179fb5aa038d7772f77a30be1c00b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-12 18:52:54 +0300

    Update tests

--- Assets/UGF.CustomSettings.Editor.Tests/TestSettingsEditor.cs
+++ Assets/UGF.CustomSettings.Editor.Tests/TestSettingsEditor.cs
@@ -7,13 +7,13 @@ namespace UGF.CustomSettings.Editor.Tests
         [Test]
         public void Package()
         {
-            Assert.AreEqual("Editor Package", TestSettingsEditorPackage.Settings.Data.Name);
+            Assert.AreEqual("Editor Package", TestSettingsEditorPackage.Settings.GetData().Name);
         }
 
         [Test]
         public void PackageExternal()
         {
-            Assert.AreEqual("Editor Package External", TestSettingsEditorPackageExternal.Settings.Data.Name);
+            Assert.AreEqual("Editor Package External", TestSettingsEditorPackageExternal.Settings.GetData().Name);
         }
     }
 }
