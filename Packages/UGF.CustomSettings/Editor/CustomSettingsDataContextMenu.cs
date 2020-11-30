3d1957bb515d22ba7a6d71f21dc2dc38592f0294
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-30 22:34:31 +0300

    Add delete context menu for settings

---
+++ Packages/UGF.CustomSettings/Editor/CustomSettingsDataContextMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UGF.CustomSettings.Runtime;
+using UnityEditor;
+
+namespace UGF.CustomSettings.Editor
+{
+    internal static class CustomSettingsDataContextMenu
+    {
+        public static event Action<CustomSettingsData> ClearMenu;
+        public static event Func<CustomSettingsData, bool> ClearValidate;
+
+        [MenuItem("CONTEXT/CustomSettingsData/Delete", false, 2000)]
+        private static void DeleteMenu(MenuCommand menuCommand)
+        {
+            if (ClearMenu != null)
+            {
+                var data = (CustomSettingsData)menuCommand.context;
+
+                ClearMenu(data);
+            }
+        }
+
+        [MenuItem("CONTEXT/CustomSettingsData/Delete", true, 2000)]
+        private static bool DeleteValidate(MenuCommand menuCommand)
+        {
+            if (ClearValidate != null)
+            {
+                var data = (CustomSettingsData)menuCommand.context;
+                bool result = ClearValidate(data);
+
+                return result;
+            }
+
+            return false;
+        }
+    }
+}
